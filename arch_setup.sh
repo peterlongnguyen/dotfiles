55cebdeadcc5513ab213221cc360f9e3d61e312b
Peter <peterlnguyen@users.noreply.github.com>
2014-08-17 19:10:36 -0700

    curled config files to append

--- arch_setup.sh
+++ arch_setup.sh
@@ -33,15 +33,25 @@ usermod -a -G sudo peter
 # set timezone
 sudo timedatectl set-timezone America/Los_Angeles
 
+# remap keys
+pacman -S xorg-xmodmap --noconfirm --quiet
+touch ~/.xmodmap
+curl https://github.com/peterlnguyen/dotfiles/blob/master/.xmodmap >> ~/.xmodmap
+
 # github setup https://help.github.com/articles/generating-ssh-keys
 pacman -S github --noconfirm --quiet
 pacman -S openssh --noconfirm --quiet
-ssh-keygen -t rsa -C "youremail@email.com"
+ssh-keygen -t rsa -C "peterlongnguyen@gmail.com"
 touch ~/.gitignore_global
+curl https://raw.githubusercontent.com/peterlnguyen/dotfiles/master/.gitignore >> ~/.gitignore_global
 git config --global core.excludesfile ~/.gitignore_global
+touch ~/.gitconfig
+wget ~/.gitconfig https://raw.githubusercontent.com/peterlnguyen/dotfiles/master/.gitconfig
 
 # i3
 pacman -S i3-wm --noconfirm --quiet
+touch ~/.i3/config
+curl https://raw.githubusercontent.com/peterlnguyen/dotfiles/master/.i3/config >> ~/.i3/config
 
 # install yaourt
 arch=$(uname -m)
@@ -50,18 +60,25 @@ echo "" >> /etc/pacman.conf
 echo "[archlinuxfr]" >> /etc/pacman.conf
 echo "Server = http://repo.archlinux.fr/$arch" >> /etc/pacman.conf
 pacman -S yaourt --noconfirm --quiet
+yaourt -Sy
 
 # vim
 pacman -S vim --noconfirm --quiet
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+touch ~/.vimrc
+curl https://raw.githubusercontent.com/peterlnguyen/dotfiles/master/.vimrc >> ~/.vimrc
 
 # bash_profile setup
 touch ~/.bash_profile
+curl https://raw.githubusercontent.com/peterlnguyen/dotfiles/master/.bash_profile >> ~/.bash_profile
 touch ~/.bashrc
+touch ~/.bash_aliases
+curl https://raw.githubusercontent.com/peterlnguyen/dotfiles/master/.bash_aliases >> ~/.bash_aliases 
 echo "set -o vi" >> ~/.bashrc
 source ~/.bash_aliases
 source ~/.bashrc
+source ~/.bash_profile
 
 # programming tools
 pacman -S irssi --noconfirm --quiet
